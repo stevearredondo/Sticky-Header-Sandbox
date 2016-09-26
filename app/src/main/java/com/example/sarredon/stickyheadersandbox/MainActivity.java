753e9490a5146a1541300af29f047e94896c02a9
Steve Arredondo <sarredon@starbucks.com>
2016-09-26 10:07:06 -0700

    Sticky Header implementation of RecyclerView.OnScrollListener.

--- app/src/main/java/com/example/sarredon/stickyheadersandbox/MainActivity.java
+++ app/src/main/java/com/example/sarredon/stickyheadersandbox/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.sarredon.stickyheadersandbox;
 
-import android.content.res.Resources;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
     Map<Type, List<String>> mMap;
 
     RecyclerView mRecyclerView;
@@ -22,34 +27,50 @@ public class MainActivity extends AppCompatActivity {
 
     Adapter mAdapter;
 
-    enum Type {
-        FAVORITES ("Favorites"),
-        PREVIOUS ("Previous"),
-        NEARBY ("Nearby");
-        public final String text;
-        Type(String text) {
-            this.text = text;
-        }
+    int headerHeight;
+    int itemHeight;
+
+    static final String[] HEADERS = new String[] {
+            "Header 1",
+            "Header 2",
+            "Header 3"
+    };
+
+    enum Type {TYPE1, TYPE2, TYPE3}
+
+    static String getText(Type type) {
+        return HEADERS[type.ordinal()];
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initMap();
+        headerHeight = getResources().getDimensionPixelOffset(R.dimen.header_height);
+        itemHeight = getResources().getDimensionPixelOffset(R.dimen.list_item_height);
         mStickyHeader = (TextView) findViewById(R.id.sticky_header);
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
+        initMap();
         mAdapter = new Adapter();
         mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.addOnScrollListener(new StickyScrollListener());
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        final int[] headerYs = computeHeaderYs(headerHeight, itemHeight, new ArrayList<Collection<?>>(mMap.values()));
+        mRecyclerView.addOnScrollListener(new StickyScrollListener(headerYs) {
+            @Override
+            protected void update(int index) {
+                Log.d(TAG, "update("+index+")");
+                mStickyHeader.setText(HEADERS[index]);
+            }
+        });
     }
 
     void initMap() {
         mMap = new EnumMap<>(Type.class);
-        mMap.put(Type.FAVORITES, Arrays.asList("fav 1", "fav 2", "fav 3"));
-        mMap.put(Type.PREVIOUS, Arrays.asList("prev 1", "prev 2", "prev 3", "prev 4"));
-        mMap.put(Type.NEARBY, Arrays.asList("nearby 1", "nearby 2", "nearby 3", "nearby 4", "nearby 5"));
+        mMap.put(Type.TYPE1, Arrays.asList("asdf", "sdfg", "afiowe", "adsdlk4", "asjdfklv", "afklsd;", "jllkj"));
+        mMap.put(Type.TYPE2, Arrays.asList("ahsfd", "asf;dj", "asdjfkl", "asdfljkk", "asd;lk", "jakls;j", "lksjdff", "ajksldf;", "dsfal", "jskldf"));
+        mMap.put(Type.TYPE3, Arrays.asList("sajdlkf", "jasdklf;", "sdjlkf", "sdlfkjsdf", "sjdlkf", "dsljdff", "sa;dj", "sld;af", "w;dafs", "as;dfkl", "zs;dflk", "wjdklds"));
     }
 
     class Adapter extends RecyclerView.Adapter<ViewHolder> {
@@ -59,9 +80,10 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View vhRoot = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
             switch (viewType) {
-                case HEADER: return new HeaderVH(parent);
-                case LIST_ITEM: return new ListItemVH(parent);
+                case HEADER: return new HeaderVH(vhRoot);
+                case LIST_ITEM: return new ListItemVH(vhRoot);
                 default: return null;
             }
         }
@@ -70,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
         public void onBindViewHolder(ViewHolder vh, int position) {
             switch (vh.getItemViewType()) {
                 case HEADER:
-                    vh.populate(getHeaderAt(position).text);
+                    vh.populate(getText(getHeaderAt(position)));
                     break;
                 case LIST_ITEM:
                     vh.populate(getListItemAt(position));
@@ -81,10 +103,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getItemCount() {
             int c = 0;
-            for (Type type : mMap.keySet()) {
-                if (!mMap.get(type).isEmpty()) {
-                    c += mMap.get(type).size() + 1;
-                }
+            for (List<String> list : mMap.values()) {
+                c += list.size() + (list.isEmpty() ? 0 : 1);
             }
             return c;
         }
@@ -136,30 +156,46 @@ public class MainActivity extends AppCompatActivity {
             }
             return null;
         }
+
     }
 
+    static int[] computeHeaderYs(int headerHeight, int itemHeight, List<Collection<?>> buckets) {
+        final int n = buckets.size();
+        final int[] untrimmedYs = new int[n];
+        int j = 0, prev = 0;
+        for (int i = 0; i < n; i++) {
+            int length = bucketSize(headerHeight, itemHeight, buckets.get(i));
+            if (length > 0) {
+                untrimmedYs[j++] = prev;
+                prev += length;
+            }
+        }
+        return Arrays.copyOf(untrimmedYs, j);
+    }
+
+    static int bucketSize(int headerHeight, int itemHeight, Collection<?> items) {
+        return (items.size() * itemHeight) + ((items.isEmpty() ? 0 : 1) * headerHeight);
+    }
 
     static abstract class ViewHolder extends RecyclerView.ViewHolder {
         protected TextView listItem;
         protected TextView header;
-        public final int height;
 
         public ViewHolder(View itemView) {
             super(itemView);
             listItem = (TextView) itemView.findViewById(R.id.normal_list_item);
             header = (TextView) itemView.findViewById(R.id.header);
-            height = getHeight(itemView.getResources());
         }
 
         abstract void populate(String text);
 
-        abstract int getHeight(Resources res);
     }
 
     static class ListItemVH extends ViewHolder {
         public ListItemVH(View itemView) {
             super(itemView);
             header.setVisibility(View.GONE);
+            listItem.setVisibility(View.VISIBLE);
         }
 
         @Override
@@ -167,15 +203,12 @@ public class MainActivity extends AppCompatActivity {
             listItem.setText(text);
         }
 
-        @Override
-        int getHeight(Resources res) {
-            return res.getDimensionPixelOffset(R.dimen.list_item_height);
-        }
     }
 
     static class HeaderVH extends ViewHolder {
         public HeaderVH(View itemView) {
             super(itemView);
+            header.setVisibility(View.VISIBLE);
             listItem.setVisibility(View.GONE);
         }
 
@@ -183,17 +216,30 @@ public class MainActivity extends AppCompatActivity {
         void populate(String text) {
             header.setText(text);
         }
-
-        @Override
-        int getHeight(Resources res) {
-            return res.getDimensionPixelOffset(R.dimen.header_height);
-        }
     }
 
-    static class StickyScrollListener extends RecyclerView.OnScrollListener {
+    abstract class StickyScrollListener extends RecyclerView.OnScrollListener {
+        final int[] headerYs;
+        final int n;
+        int y = 0;
+        int _i = -1;
+        public StickyScrollListener(int[] headerYs) {
+            n = headerYs.length;
+            this.headerYs = Arrays.copyOf(headerYs, n);
+        }
+
         @Override
         public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-
+            y += dy;
+            for (int i = 0; i < n; i++) {
+                int yi = headerYs[i];
+                if (yi <= y && (i == n - 1 || y < headerYs[i+1]) && _i != i) {
+                    _i = i;
+                     update(i);
+                }
+            }
         }
+
+        protected abstract void update(int index);
     }
 }
